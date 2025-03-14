1cf55e19b9672adb9ce05b308ce13fd5cb74d2da
Kirill Belyaev <kirill10121415@gmail.com>
2025-03-15 00:50:44 +0300

    postgres configs added

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -1,1 +1,12 @@
 package postgres
+
+// no env vars
+// add env vars
+
+type Config struct {
+	Host     string `yaml:"POSTGRES_HOST" env:"POSTGRES_HOST" env-default:"localhost"`
+	Port     string `yaml:"POSTGRES_HOST" env:"POSTGRES_HOST" env-default:"localhost"`
+	Username string `yaml:"POSTGRES_HOST" env:"POSTGRES_HOST" env-default:"localhost"`
+	Password string `yaml:"POSTGRES_HOST" env:"POSTGRES_HOST" env-default:"localhost"`
+	Database string `yaml:"POSTGRES_HOST" env:"POSTGRES_HOST" env-default:"localhost"`
+}
