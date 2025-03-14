fdcbcf9556336e7e1d2c2ade81839c1a5a37c590
Kirill Belyaev <kirill10121415@gmail.com>
2025-03-15 00:50:29 +0300

    zap logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,51 @@
 package logger
+
+import (
+	"context"
+	"go.uber.org/zap"
+)
+
+const (
+	Key       = "Logger"
+	RequestID = "request_id"
+)
+
+var logger *zap.Logger
+
+type Logger struct {
+	l *zap.Logger
+}
+
+func NewLogger(ctx context.Context) (context.Context, error) {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		return nil, err
+	}
+	ctx = context.WithValue(ctx, Key, &Logger{logger})
+	return ctx, nil
+}
+
+func GetLoggerFromCtx(ctx context.Context) *Logger {
+	return ctx.Value(Key).(*Logger)
+}
+
+func (l *Logger) Info(ctx context.Context, msg string, fields ...zap.Field) {
+	if ctx.Value(RequestID) != nil {
+		fields = append(fields, zap.String(RequestID, ctx.Value(RequestID).(string)))
+	}
+	l.l.Info(msg, fields...)
+}
+
+func (l *Logger) Error(ctx context.Context, msg string, fields ...zap.Field) {
+	if ctx.Value(RequestID) != nil {
+		fields = append(fields, zap.String(RequestID, ctx.Value(RequestID).(string)))
+	}
+	l.l.Error(msg, fields...)
+}
+
+func (l *Logger) Fatal(ctx context.Context, msg string, fields ...zap.Field) {
+	if ctx.Value(RequestID) != nil {
+		fields = append(fields, zap.String(RequestID, ctx.Value(RequestID).(string)))
+	}
+	l.l.Fatal(msg, fields...)
+}
