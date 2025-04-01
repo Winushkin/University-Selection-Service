2976a9c3b409cb47976f536a7bb117975997ad34
lazarevFedor <lazarevfedor@gmail.com>
2025-04-01 18:09:48 +0300

    feat: initialize server with context and logger integration

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"University-Selection-Service/internal/server"
+	logger2 "University-Selection-Service/pkg/logger"
+	"context"
+)
 
+func main() {
+	ctx := context.Background()
+	ctx, _ = logger2.NewLogger(ctx)
+	server.StartServer(ctx)
 }
