c05b1ef7af13f75fc4c3b902860469b96aee28b8
yulia <balevajued@gmail.com>
2025-04-22 00:36:32 +0300

    подобие формы входа с жалкими попытками взаимодействия с сервером по образу и подобию Фединого файла

---
+++ frontend/src/pages/LoginForm.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import  "./RegistrationForm.css"
+import styles from "./Home.module.css";
+import logo from "./logo.png";
+
+
+function LoginForm() {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        /*
+                const foundUser = user.find (
+            (user) => user.login === login && user.password === password
+        );
+
+        if (!foundUser) {
+            setError('Неверный логин или пароль');
+            return;
+        }
+
+        console.log('Вход выполнен:', login);
+        navigate('/'); //тут должна быть главная страница
+
+            */
+    };
+
+    return (
+        <div>
+            <div className={styles.header}>
+                <div className={styles.logo}>
+                    <img src={logo} alt="Logo" className={styles.logoImage}/>
+                </div>
+                <div className={styles.titleWrapper}>
+                    <div className={styles.title}>UniQuest</div>
+                    <div className={styles.subtitle}>Найдите идеальный университет для себя</div>
+                </div>
+            </div>
+
+            <div className="register-container">
+                <h2>Вход</h2>
+                <form onSubmit={handleLogin}>
+                    <div>
+                        <label>Логин:</label>
+                        <input
+                            type="text"
+                            value={login}
+                            onChange={(e) => setLogin(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label>Пароль:</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    {error && <p style={{color: 'red'}}>{error}</p>}
+                    <button type="submit">Войти</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
+
+
+
