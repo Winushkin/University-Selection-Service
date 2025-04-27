1a5a0bf45ae0432fe9d54c0a9bb41918bf1a0288
lazarevFedor <lazarevfedor3@gmail.com>
2025-04-27 14:04:49 +0300

    rename: from config.go

--- backend/internal/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import (
-	"University-Selection-Service/pkg/postgres"
-)
-
-type Config struct {
-	postgres.Config `yaml:"POSTGRES" env:"POSTGRES"`
-}
---
+++ backend/internal/config/user_config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"University-Selection-Service/pkg/postgres"
+	"errors"
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type UserConfig struct {
+	postgres.Config `env:"POSTGRES"`
+	INTPort         string `env:"INT_PORT"`
+	RESTPort        string `env:"REST_PORT"`
+}
+
+func New() (*UserConfig, error) {
+	err := godotenv.Load()
+	if !errors.Is(err, os.ErrNotExist) && err != nil {
+		return nil, fmt.Errorf("NewUserConfig: error: %w", err)
+	}
+
+	var cfg UserConfig
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		return nil, fmt.Errorf("NewUserConfig: reading env error: %w", err)
+	}
+	return &cfg, nil
+}
