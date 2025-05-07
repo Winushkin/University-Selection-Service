d61adde8679b0708de99d838a7c7f9eb45fdcb06
Kirill Belyaev <kirill10121415@gmail.com>
2025-05-07 23:30:15 +0300

    add: University entity

---
+++ backend/internal/entities/university.go
@@ -0,0 +1,17 @@
+package entities
+
+type University struct {
+	Id          int
+	Prestige    int
+	Name        string
+	Rank        float64
+	Speciality  string
+	Quality     int
+	Points      float64
+	Cost        int
+	Dormitory   bool
+	Labs        bool
+	Sport       bool
+	Scholarship int
+	Region      string
+}
