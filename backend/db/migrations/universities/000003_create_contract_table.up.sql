ee4bbdd15904079ebd6f87245901c1ab1b2b041c
Kirill Belyaev <kirill10121415@gmail.com>
2025-05-07 23:17:17 +0300

    add: new migrations for universities db

---
+++ backend/db/migrations/universities/000003_create_contract_table.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE if not exists universities.contract(
+    id SERIAL PRIMARY KEY,
+    prestige INT NOT NULL,
+    name TEXT NOT NULL,
+    rank FLOAT NOT NULL,
+    speciality TEXT NOT NULL,
+    quality SMALLINT NOT NULL,
+    points FLOAT NOT NULL,
+    cost INT NOT NULL,
+    dormitory BOOLEAN,
+    labs BOOLEAN,
+    sport BOOLEAN,
+    scholarship INT,
+    region_name TEXT NOT NULL
+);
+
+
