40369b9d6aed24a60674948a2b140245f954c97b
lazarevFedor <fedos1611@yandex.ru>
2025-05-08 21:27:19 +0300

    add: interaction with profile data between user service and analytic service, Analyze request handler start realization

--- backend/internal/analytic/server/server.go
+++ backend/internal/analytic/server/server.go
@@ -10,15 +10,31 @@ import (
 
 type Server struct {
 	api.AnalyticServer
-	rep *repositories.AnalyticRepository
+	rep     *repositories.AnalyticRepository
+	userCli api.UserServiceClient
 }
 
-func New(ctx context.Context, cfg *config.AnalyticConfig) (*Server, error) {
+func New(ctx context.Context, cfg *config.AnalyticConfig, client api.UserServiceClient) (*Server, error) {
 	r, err := repositories.NewAnalyticRepository(ctx, cfg.Postgres)
 	if err != nil {
 		return nil, fmt.Errorf("NewAnalyticRepository: %w", err)
 	}
 	return &Server{
-		rep: r,
+		rep:     r,
+		userCli: client,
 	}, nil
+}
+
+func (s *Server) Analyze(ctx context.Context, request *api.AnalyzeRequest) (*api.AnalyzeResponse, error) {
+	id, ok := ctx.Value("id").(int)
+	if !ok {
+		return nil, fmt.Errorf("id is not int")
+	}
+	_, err := s.userCli.ProfileDataForAnalytic(ctx, &api.ProfileDataForAnalyticRequest{
+		Id: int32(id),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("ProfileDataForAnalytic: %w", err)
+	}
+	return nil, nil
 }
