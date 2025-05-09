ef84c250a04408fac5f1c85887dbc273ab483a6e
Kirill Belyaev <kirill10121415@gmail.com>
2025-05-09 04:40:38 +0300

    edit: university entity add: speciality entity

---
+++ backend/internal/entities/speciality.go
@@ -0,0 +1,10 @@
+package entities
+
+type Speciality struct {
+	Id             int
+	UniversityName string
+	Name           string
+	BudgetPoints   float64
+	ContractPoints float64
+	Cost           int
+}
--- backend/internal/entities/university.go
+++ backend/internal/entities/university.go
@@ -1,17 +1,18 @@
 package entities
 
 type University struct {
-	Id          int
-	Prestige    int
-	Name        string
-	Rank        float64
-	Speciality  string
-	Quality     int
-	Points      float64
-	Cost        int
-	Dormitory   bool
-	Labs        bool
-	Sport       bool
-	Scholarship int
-	Region      string
+	Id             int
+	Prestige       int
+	Name           string
+	Site           string
+	Rank           float64
+	Quality        int
+	ContractPoints float64
+	BudgetPoints   float64
+	Cost           int
+	Dormitory      bool
+	Labs           bool
+	Sport          bool
+	Scholarship    int
+	Region         string
 }
