7c503910f27178c9bb6bb0178b9093e021634fa1
yulia <balevajued@gmail.com>
2025-05-10 05:39:32 +0300

    теперь проще отправлять запросы меньше буков

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // любой запрос, начинающийся с /api, будет проксироваться на http://localhost:80
+      '/api': {
+        target: 'http://localhost:80',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
